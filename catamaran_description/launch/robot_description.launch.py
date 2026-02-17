736a5a8963a88951b703c3a624f4f035acb56f1e
Guillem Fustier <al426661@uji.es>
2026-02-17 17:41:44 +0100

    robot_description launch modificado

--- catamaran_description/launch/robot_description.launch.py
+++ catamaran_description/launch/robot_description.launch.py
@@ -2,6 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare 
+from launch_ros.parameter_descriptions import ParameterValue
+
 
 def generate_launch_description():
     xacro_file = PathJoinSubstitution([
@@ -16,7 +18,7 @@ def generate_launch_description():
             executable="robot_state_publisher",
             name="robot_state_publisher",
             parameters=[{
-                "robot_description": Command(["xacro ", xacro_file])
+                'robot_description': ParameterValue(Command(['xacro ', xacro_file]), value_type=str)
             }]
         )
     ])
