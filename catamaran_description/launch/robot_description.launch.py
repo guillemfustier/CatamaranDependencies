71003ea22b18bf13ba42fd55d4ac87dd57c01335
Guillem Fustier <al426661@uji.es>
2026-02-17 17:36:46 +0100

    robot_description launch modificado

--- catamaran_description/launch/robot_description.launch.py
+++ catamaran_description/launch/robot_description.launch.py
@@ -1,25 +1,22 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare 
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('catamaran_description')
-    
-    # Path to Xacro file
-    xacro_file = os.path.join(pkg_share, 'urdf', 'catamaran.xacro')
+    xacro_file = PathJoinSubstitution([
+        FindPackageShare("catamaran_description"),
+        "urdf",
+        "catamaran.xacro"
+    ])
 
     return LaunchDescription([
         Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
             parameters=[{
-                'robot_description': ParameterValue(Command(['xacro ', xacro_file]), value_type=str)
+                "robot_description": Command(["xacro ", xacro_file])
             }]
         )
     ])
