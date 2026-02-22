c95620df1c84e47ad5082b1e45a6b3fe77608985
Guillem Fustier <al426661@uji.es>
2026-02-22 11:01:26 +0100

    Debug nuc

--- catamaran_core/launch/nuc_debug.launch.py
+++ catamaran_core/launch/nuc_debug.launch.py
@@ -1,11 +1,9 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch_ros.actions import Node, PushRosNamespace
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     pkg_share_sensors = get_package_share_directory('catamaran_sensors')
@@ -27,17 +25,7 @@ def generate_launch_description():
             ),
 
             # ------------------------------------
-            # 2. CMD Vel Translator - NUC
-            # ------------------------------------
-            Node(
-                package='motor_bringup',
-                executable='cmdvel_motor_controller',
-                name='cmdvel_motor_controller',
-                output='screen'
-            ),
-
-            # ------------------------------------
-            # 3. Publicar GPS (Ublox + NTRIP) - NUC
+            # 2. Publicar GPS (Ublox + NTRIP) - NUC
             # ------------------------------------
             # Driver Node (Talks to USB)
             Node(
@@ -76,36 +64,27 @@ def generate_launch_description():
                 }]
             ),
 
-            # # ------------------------------------
-            # # 4. Robot Description - NUC
-            # # ------------------------------------
-            # IncludeLaunchDescription(
-            #     PythonLaunchDescriptionSource([
-            #         FindPackageShare("catamaran_description"), '/launch/robot_description.launch.py'
-            #     ])
-            # ),
-
-            # # ------------------------------------
-            # # 5. Cámara Siyi - NUC
-            # # ------------------------------------
-            # Node(
-            #     package='zr30camera',
-            #     executable='controller',
-            #     name='zr30_controller',
-            #     output='screen'
-            # ),
-            # Node(
-            #     package='zr30camera',
-            #     executable='stream',
-            #     name='zr30_stream',
-            #     output='screen',
-            #     remappings=[
-            #         ('/ZR30/camera_stream', 'siyi_camera/image_raw') # Relativo: /catamaran/siyi_camera/...
-            #     ]
-            # ),
+            # ------------------------------------
+            # 3. Cámara Siyi - NUC
+            # ------------------------------------
+            Node(
+                package='zr30camera',
+                executable='controller',
+                name='zr30_controller',
+                output='screen'
+            ),
+            Node(
+                package='zr30camera',
+                executable='stream',
+                name='zr30_stream',
+                output='screen',
+                remappings=[
+                    ('/ZR30/camera_stream', 'siyi_camera/image_raw') # Relativo: /catamaran/siyi_camera/...
+                ]
+            ),
 
             # ------------------------------------
-            # 6. Cámara USB: - NUC
+            # 4. Cámara USB: - NUC
             # ------------------------------------
             Node(
                 package='usb_cam',
