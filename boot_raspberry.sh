1938fa8a4d6c4d0bd969c12dfbcc7b1b8dd2a14b
Guillem Fustier <al426661@uji.es>
2026-02-23 09:50:15 +0100

    Parametros origen lat, lon

--- boot_raspberry.sh
+++ boot_raspberry.sh
@@ -10,4 +10,12 @@ ros2 launch mavros apm.launch fcu_url:=udp://127.0.0.1:14551@
 
 
 
-ros2 launch catamaran_sensors mavlink_tf.launch.py
+
+
+# Usa argumentos $1 y $2 si existen, sino usa valores por defecto
+LAT_ORIGIN=${1:-39.99446582}
+LON_ORIGIN=${2:--0.07405792}
+
+echo "Iniciando con origen GPS: Lat=$LAT_ORIGIN, Lon=$LON_ORIGIN"
+
+ros2 launch catamaran_sensors mavlink_tf.launch.py origin_lat:=$LAT_ORIGIN origin_lon:=$LON_ORIGIN
--- catamaran_sensors/launch/mavlink_tf.launch.py
+++ catamaran_sensors/launch/mavlink_tf.launch.py
@@ -1,9 +1,22 @@
 import os
 import launch
 import launch_ros.actions
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return launch.LaunchDescription([
+        DeclareLaunchArgument(
+            'origin_lat',
+            default_value='39.99446582',
+            description='Latitude origin'
+        ),
+        DeclareLaunchArgument(
+            'origin_lon',
+            default_value='-0.07405792',
+            description='Longitude origin'
+        ),
+
         # Nodo MAVLink Bridge
         launch_ros.actions.Node(
             package='catamaran_sensors',
@@ -22,10 +35,10 @@ def generate_launch_description():
             executable='tf_gps_publisher',
             name='tf_gps_publisher',
             output='screen',
-            # parameters=[{
-            #     'origin_lat': 39.99446582,
-            #     'origin_lon': -0.07405792
-            # }]
+            parameters=[{
+                'origin_lat': LaunchConfiguration('origin_lat'),
+                'origin_lon': LaunchConfiguration('origin_lon')
+            }]
         )
     ])
 
